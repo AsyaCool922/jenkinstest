173f08bb07bac3ed3f07195774c812c43e1fafa1
asya <asyacool922@gmail.com>
2024-03-28 16:58:31 +0300

    test02

--- src/test/java/tests/Google.java
+++ src/test/java/tests/Google.java
@@ -3,13 +3,14 @@ package tests;
 import org.testng.annotations.Test;
 
 import static utilities.Driver.driver;
+import static utilities.Driver.getDriver;
 
 public class Google {
     @Test
     public void test01(){
-
         System.out.println("test01 start");
         driver.get("https://www.google.com/");
+        getDriver().close();
     }
 
 
