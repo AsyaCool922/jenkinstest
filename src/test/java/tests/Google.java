21f29cd14910a148eaa1e4e3956bf29aa1cc495f
asya <asyacool922@gmail.com>
2024-03-28 15:40:22 +0300

    test01

---
+++ src/test/java/tests/Google.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static utilities.Driver.driver;
+
+public class Google {
+    @Test
+    public void test01(){
+
+        System.out.println("test01 start");
+        driver.get("https://www.google.com/");
+    }
+
+
+}
